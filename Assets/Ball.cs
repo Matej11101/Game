3139039a093518b5808e116530a82f4611c5d69b
Mateusz_Ch <476594@chrome.sussexdowns.ac.uk>
2019-12-04 23:16:13 +0000

    Beginning

---
+++ Assets/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float speed;
+    public Transform explosion;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+
+        if (Input.GetButtonDown("Jump") && !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * speed);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bottom"))
+        {
+            Debug.Log("Ball hit the Bottom");
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Brick"))
+        {
+            Transform newExplosion = Instantiate(explosion, other.transform.position, other.transform.rotation);
+            Destroy(newExplosion.gameObject, 2.5f);
+            Destroy(other.gameObject);
+        }
+    }
+}
